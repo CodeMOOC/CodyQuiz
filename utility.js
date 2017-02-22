623a920e93792073711cadce4562b713b9fa4e6e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-02-22 11:37:24 +0100

    Support more special characters in normalization

--- utility.js
+++ utility.js
@@ -1,11 +1,13 @@
 module.exports = function() {
     String.prototype.normalizeResponse = function() {
-        return this.toString().trim().toLocaleLowerCase()
-            .replace(/à|á|ä|â/g, 'a')
-            .replace(/è|é|ê/g, 'e')
+        return this.toString().toLocaleLowerCase()
+            .replace(/à|á|ä|â|å|ã|ā|ă|ą/g, 'a')
+            .replace(/è|é|ê|ë|ē|ė|ę|ě/g, 'e')
             .replace(/ù|ú|ü|û/g, 'u')
-            .replace(/ò|ó|ö|ô/g, 'o')
-            .replace(/ì|í|î/g, 'i')
+            .replace(/ò|ó|ö|ô|õ/g, 'o')
+            .replace(/ì|í|î|ï|ĩ|ī|ĭ/g, 'i')
+            .replace(/[^a-z0-9]/g, ' ')
+            .trim()
         ;
     };
 }
